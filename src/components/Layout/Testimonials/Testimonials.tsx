ecdab6905b2c3b235dc3d070a158c81891f99946
Jesse Lind <Jesseclind@gmail.com>
2025-03-31 17:43:35 -0400

    feat(pricing): add pricing page factors component section

---
+++ src/components/Layout/Testimonials/Testimonials.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Testimonials.module.scss'
+
+const Testimonials = () => {
+  return (
+    <div className={styles.Testimonials}>
+      <div className='page__inner'>
+        <div className={styles.content}>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials
---
+++ src/components/Pages/Pricing/PricingFactors/PricingFactors.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styles from './PricingFactors.module.scss'
+import { ButtonLink } from '../../../Common/ButtonLink'
+
+const PricingFactors = () => {
+  return (
+    <div className={styles.PricingFactors}>
+      <div className='page__inner'>
+        <div className={styles.content}>
+          <div className='image'>
+            <img src='/images/pricing/pricing.webp' alt='' />
+          </div>
+          <div className={styles.text_content}>
+            <div className={styles.section}>
+              <h2>FACTORS THAT INFLUENCE PRICING</h2>
+              <ul>
+                <li>
+                  <strong>Project Size:</strong> Larger renovations require more
+                  materials and labor.
+                </li>
+                <li>
+                  <strong>Materials & Finishes:</strong> The quality and type of
+                  materials impact the overall cost.
+                </li>
+                <li>
+                  <strong>Customization & Complexity:</strong> Unique design
+                  elements or structural changes can add to the price.
+                </li>
+              </ul>
+            </div>
+            <div className={styles.section}>
+              <h2>NO-OBLIGATION ESTIMATES</h2>
+              <p>
+                Since every project is unique, we provide personalized estimates
+                based on your specific needs. <a href='/contact'>Contact us</a>{' '}
+                today to schedule a free, no-obligation consultation.
+              </p>
+            </div>
+            <ButtonLink
+              text='Get Your Free Estimate'
+              link='contact'
+              color='secondary'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PricingFactors
