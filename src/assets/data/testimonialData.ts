6aa0a55fd8852112c0f0719e7ba9fa0853d5d8ac
Jesse Lind <Jesseclind@gmail.com>
2025-03-31 18:45:38 -0400

    feat(testimonials): create and add responsiveness to testimonials component

---
+++ src/assets/data/testimonialData.ts
@@ -0,0 +1,25 @@
+import { string } from 'astro:schema'
+
+export interface TestimonialType {
+  name: string
+  rating: 1 | 2 | 3 | 4 | 5
+  text: string
+}
+
+export const testimonials: TestimonialType[] = [
+  {
+    name: 'Hannah H.',
+    rating: 5,
+    text: 'Tim has excellent craftsmanship, is super easy to communicate with, and is a very trustworthy and honest businessman! For any bathroom or other handwork related to homes, Tim is a top choice!  - His expertise and integrity make him highly valued in his industry.',
+  },
+  {
+    name: 'Mark T.',
+    rating: 5,
+    text: 'TLC is a fine builder and remodeler! I’ve used this company for bathroom remodeling and a custom post build on the front exterior of my house, which came out extraordinarily nice. Tim Lewis does high-end work meticulously well. Tim is also very honest, punctual and reliable. For construction work, TLC is the company to call.',
+  },
+  {
+    name: 'Emily L.',
+    rating: 5,
+    text: 'I love TLC! He is very professional and finishes the job beautifully! His finish work is pristine. He opened up our small bathroom and made it larger and tiled a curbless shower! He put in a sky light in the shower and a larger window near the vanity for natural light. It’s gorgeous!',
+  },
+]
--- src/components/Layout/Testimonials/Testimonials.tsx
+++ src/components/Layout/Testimonials/Testimonials.tsx
@@ -1,11 +1,51 @@
 import React from 'react'
 import styles from './Testimonials.module.scss'
+import {
+  testimonials,
+  type TestimonialType,
+} from '../../../assets/data/testimonialData'
+import { Quote, Star } from 'lucide-react'
+
+const SingleTestimonial = ({
+  testimonial,
+}: {
+  testimonial: TestimonialType
+}) => {
+  const { name, rating, text } = testimonial
+  return (
+    <div className={`${styles.SingleTestimonial} card`}>
+      <div className={styles.quote_icon}>
+        <Quote fill='var(--primary-accent)' strokeWidth={1} />
+      </div>
+      <p className={styles.text}>{text}</p>
+      <div className={styles.bottom}>
+        <p className={styles.name}>{name}</p>
+        <div className={styles.rating}>
+          {Array.from({ length: rating }, (_, i) => (
+            <span key={i} className={styles.star}>
+              <Star size={20} fill='var(--secondary-accent)' strokeWidth={0} />
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const Testimonials = () => {
   return (
     <div className={styles.Testimonials}>
       <div className='page__inner'>
         <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.subtitle}>TESTIMONIALS</div>
+            <h1 className='section-title'>What Our Clients Say</h1>
+          </div>
+          <div className={styles.testimonials_list}>
+            {testimonials.map((testimonial, index) => (
+              <SingleTestimonial key={index} testimonial={testimonial} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
