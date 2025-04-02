147145e9db8cfc712fa570fdaffc0aa1dc8bdffa
Jesse Lind <Jesseclind@gmail.com>
2025-04-02 18:20:04 -0400

    feat(services): add RenovationProcess component boilierplate

---
+++ src/components/Pages/Services/RenovationProcess/RenovationProcess.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './RenovationProcess.module.scss'
+
+const RenovationProcess = () => {
+  return (
+    <div className={styles.RenovationProcess}>
+      <div className='page__inner'>
+        <div className={styles.content}>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RenovationProcess
