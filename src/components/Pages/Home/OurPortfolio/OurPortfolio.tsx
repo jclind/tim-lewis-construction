5a3025a17e1eb84e4a7291cbbdc2da094465d98a
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-04-04 16:04:27 -0400

    feat(home): add OurPortfolio boilerplate

---
+++ src/components/Pages/Home/OurPortfolio/OurPortfolio.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './OurPortfolio.module.scss'
+
+const OurPortfolio = () => {
+  return (
+    <div className={styles.OurPortfolio}>
+      <div className='page__inner'>
+        <div className={styles.content}>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default OurPortfolio
