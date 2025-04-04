c1caa0bc7039a5c9770ef3bee61ff4d4dc248ae3
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-04-04 15:56:06 -0400

    feat(home): add responsiveness with image to AboutUs component

--- src/components/Pages/Home/AboutUs/AboutUs.tsx
+++ src/components/Pages/Home/AboutUs/AboutUs.tsx
@@ -34,45 +34,42 @@ const AboutUs = () => {
           <div className={`${styles.image} image`}>
             <img src='/images/shared/tlc_card.webp' alt='' />
           </div>
-          <div className={styles.about_us_text}>
-            <h1 className='section-subtitle'>ABOUT US</h1>
-            <h2 className='section-title'>
-              Built on Quality, Driven by Integrity
-            </h2>
-            <p className='section-description'>
-              At Tim Lewis Construction, we bring years of hands-on experience
-              and a passion for craftsmanship to every project. From small
-              repairs to full-scale renovations, we believe every home deserves
-              the same attention to detail and care.
-            </p>
-          </div>
-          <div className={styles.why_choose_us_text}>
-            <h1 className='section-subtitle'>WHY CHOOSE US?</h1>
-            <div className={styles.values}>
-              {whyChooseUsList.map((item, index) => {
-                const Icon = item.icon
-                return (
-                  <div className={styles.item} key={index}>
-                    <Icon
-                      className={styles.icon}
-                      size={
-                        item.title === 'Serving Your Neighborhood' ? 36 : 26
-                      }
-                    />
-                    <div className={styles.text}>
-                      <h2>{item.title}</h2>
-                      <p dangerouslySetInnerHTML={{ __html: item.text }}></p>
-                    </div>
-                  </div>
-                )
-              })}
+          <div className={styles.text_content}>
+            <div className={styles.about_us_text}>
+              <h1 className='section-subtitle'>ABOUT US</h1>
+              <h2 className='section-title'>
+                Built on Quality, Driven by Integrity
+              </h2>
+              <p className='section-description'>
+                At Tim Lewis Construction, we bring years of hands-on experience
+                and a passion for craftsmanship to every project. From small
+                repairs to full-scale renovations, we believe every home
+                deserves the same attention to detail and care.
+              </p>
             </div>
+            <div className={styles.why_choose_us_text}>
+              <h1 className='section-subtitle'>WHY CHOOSE US?</h1>
+              <div className={styles.values}>
+                {whyChooseUsList.map((item, index) => {
+                  const Icon = item.icon
+                  return (
+                    <div className={styles.item} key={index}>
+                      <Icon className={styles.icon} size={24} />
+                      <div className={styles.text}>
+                        <h2>{item.title}</h2>
+                        <p dangerouslySetInnerHTML={{ __html: item.text }}></p>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+            <ButtonLink
+              text="Let's Get In Touch"
+              link='contact'
+              color='secondary'
+            />
           </div>
-          <ButtonLink
-            text="Let's Get In Touch"
-            link='contact'
-            color='secondary'
-          />
         </div>
       </div>
     </div>
